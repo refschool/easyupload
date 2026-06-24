2c0eb31fafc4bb1bb5f1b58ed65dba6b079e0e95
arnaudbregere <arnaud.bregere@gmail.com>
2026-06-24 15:58:27 +0200

    Social networks : create new social share php component + css custom DL button + social network buttons

--- src/Views/pages/download.php
+++ src/Views/pages/download.php
@@ -2,21 +2,30 @@
 
 <?php if (!empty($error)): ?>
 
-    <p style="color:red;">
+    <p class="download__error">
         <?= \App\Core\View::escape($error) ?>
     </p>
 
 <?php elseif (!empty($file)): ?>
 
-    <p>
-        Téléchargement du fichier :
-        <strong><?= \App\Core\View::escape($file) ?></strong>
+    <p class="download__filename">
+        Fichier : <strong><?= \App\Core\View::escape($file) ?></strong>
     </p>
 
-    <a class="btn btn-primary"
-       href="/download/file?file=<?= urlencode($file) ?>">
+    
+    <a class="btn btn-primary download__btn"
+        href="/download/file?file=<?= urlencode($file) ?>"
+        aria-label="Télécharger le fichier <?= \App\Core\View::escape($file) ?>"
+        download
+    >
+        <i class="bi bi-download"></i>
         Télécharger
     </a>
+
+    <?php \App\Core\View::partial('components/social-share', [
+        'shareUrl'  => $_ENV['WEB_URL'] . 'download?file=' . urlencode($file),
+        'shareText' => 'Je viens de recevoir des fichiers via EasyUpload ! Télécharge-les ici :',
+    ]); ?>
 
 <?php else: ?>
 
